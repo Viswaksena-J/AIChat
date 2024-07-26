46b0d8c353bce2810c233242339e98017007a142
Viswaksena Jayam <viswaksene111@gmail.com>
2024-07-26 14:18:22 +0530

    Typing effect animation(Homepage)

--- src/routes/homepage/Homepage.jsx
+++ src/routes/homepage/Homepage.jsx
@@ -1,7 +1,11 @@
 import { Link } from "react-router-dom";
 import "./homepage.css";
+import { TypeAnimation } from "react-type-animation";
+import { useState } from "react";
 
 const Homepage = () => {
+  const [typingStatus, setTypingStatus] = useState("human");
+
   return (
     <div className="homepage">
       <img src="/orbital.png" alt="" className="orbital" />
@@ -20,6 +24,56 @@ const Homepage = () => {
             <div className="bg"></div>
           </div>
           <img src="/bot.png" alt="" className="bot" />
+          <div className="chat">
+            <img
+              src={
+                typingStatus === "human1"
+                  ? "/human1.jpeg"
+                  : typingStatus === "bot"
+                  ? "/bot.png"
+                  : "human2.jpeg"
+              }
+              alt=""
+            />
+            <TypeAnimation
+              sequence={[
+                // Same substring at the start will only be typed out once, initially
+                "Human: We produce food for Mice",
+                2000,
+                () => {
+                  setTypingStatus("bot");
+                },
+                "Bot: We produce food for Hamsters",
+                2000,
+                () => {
+                  setTypingStatus("human2");
+                },
+                "Human2: We produce food for Guinea Pigs",
+                2000,
+                () => {
+                  setTypingStatus("bot");
+                },
+                "Bot: We produce food for Chinchillas",
+                2000,
+                () => {
+                  setTypingStatus("human1");
+                },
+              ]}
+              wrapper="span"
+              // speed={50}
+              cursor={true}
+              omitDeletionAnimation={true}
+              repeat={Infinity}
+            />
+          </div>
+        </div>
+      </div>
+      <div className="terms">
+        <img src="/logo.png" alt="" />
+        <div className="links">
+          <Link>Terms of Service</Link>
+          <span>|</span>
+          <Link>Privacy Policy</Link>
         </div>
       </div>
     </div>
