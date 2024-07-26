ea15fa90714986a845f52b96f0da89ff587b36e0
Viswaksena Jayam <viswaksene111@gmail.com>
2024-07-26 23:07:24 +0530

    Prompting Design

---
+++ src/components/newPrompt/NewPrompt.jsx
@@ -0,0 +1,29 @@
+import "./newPrompt.css";
+import { useRef } from "react";
+import { useEffect } from "react";
+
+const NewPrompt = () => {
+  const endRef = useRef(null);
+
+  useEffect(() => {
+    endRef.current.scrollIntoView({ behavior: "smooth" });
+  }, []);
+  return (
+    <>
+      Test
+      <div className="endChat" ref={endRef}></div>
+      <form className="newForm">
+        <label htmlFor="file">
+          <img src="/attachment.png" alt="" />
+        </label>
+        <input id="file" type="file" multiple={false} hidden />
+        <input type="text" placeholder="Ask me anything...." />
+        <button>
+          <img src="/arrow.png" alt="" />
+        </button>
+      </form>
+    </>
+  );
+};
+
+export default NewPrompt;
--- src/routes/chatpage/Chatpage.jsx
+++ src/routes/chatpage/Chatpage.jsx
@@ -1,9 +1,38 @@
-import './chatpage.css'
+import "./chatpage.css";
+import NewPrompt from "../../components/newPrompt/NewPrompt";
 
 const Chatpage = () => {
+  
   return (
-    <div className='chatpage'>Chatpage</div>
-  )
-}
+    <div className="chatpage">
+      <div className="wrapper">
+        <div className="chat">
+          <div className="message">Test Message from ai</div>
+          <div className="message user">Test Message from user Lorem ipsum dolor sit amet, consectetur adipisicing elit. Facilis rem, dolores libero nisi tempore animi, veniam ratione culpa est in recusandae laborum architecto sed eligendi praesentium consequuntur dolor, delectus itaque.
+          Reiciendis vel soluta quaerat unde explicabo, dolores obcaecati. Laboriosam deserunt expedita consectetur voluptatem illum dolorem, quo commodi beatae aspernatur non sed voluptatibus error fugiat obcaecati, rerum cum nemo libero eligendi.</div>
+          <div className="message">Test Message from ai</div>
+          <div className="message user">Test Message from user</div>
+          <div className="message">Test Message from ai</div>
+          <div className="message user">Test Message from user</div>
+          <div className="message">Test Message from ai</div>
+          <div className="message user">Test Message from user</div>
+          <div className="message">Test Message from ai</div>
+          <div className="message user">Test Message from user</div>
+          <div className="message">Test Message from ai</div>
+          <div className="message user">Test Message from user</div>
+          <div className="message">Test Message from ai</div>
+          <div className="message user">Test Message from user</div>
+          <div className="message">Test Message from ai</div>
+          <div className="message user">Test Message from user</div>
+          <div className="message">Test Message from ai</div>
+          <div className="message user">Test Message from user</div>
+          <div className="message">Test Message from ai</div>
+          <div className="message user">Test Message from user</div>
+          <NewPrompt/>
+        </div>
+      </div>
+    </div>
+  );
+};
 
-export default Chatpage
+export default Chatpage;
