cda3ba12dec6cf78e8907bf723af323fb9b22d94
Viswaksena Jayam <viswaksene111@gmail.com>
2024-07-30 01:04:56 +0530

    AI Chat using gemini

--- client/src/components/newPrompt/NewPrompt.jsx
+++ client/src/components/newPrompt/NewPrompt.jsx
@@ -3,8 +3,12 @@ import { useRef, useState } from "react";
 import { useEffect } from "react";
 import Upload from "../upload/Upload";
 import { IKImage } from "imagekitio-react";
+import model from "../../lib/gemini";
+import Markdown from "react-markdown";
 
 const NewPrompt = () => {
+  const [question, setQuestion] = useState("");
+  const [answer, setAnswer] = useState("");
   const [img, setImg] = useState({
     isLoading: false,
     error: "",
@@ -15,7 +19,26 @@ const NewPrompt = () => {
 
   useEffect(() => {
     endRef.current.scrollIntoView({ behavior: "smooth" });
-  }, []);
+  }, [question, answer, img.dbData]);
+
+  const add = async (text) => {
+    setQuestion(text);
+
+    const result = await model.generateContent(text);
+    const response = await result.response;
+    setAnswer(response.text());
+    console.log(text);
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    const text = e.target.text.value;
+    if (!text) return;
+
+    add(text);
+  };
+
   return (
     <>
       {img.isLoading && <div className="loading">Loading...</div>}
@@ -27,14 +50,21 @@ const NewPrompt = () => {
           transformation={[{ width: 380 }]}
         />
       )}
+      {question && <div className="message user">{question}</div>}
+      {answer && (
+        <div className="message">
+          <Markdown>{answer}</Markdown>
+        </div>
+      )}
+      <button onClick={add}>Test me</button>
       <div className="endChat" ref={endRef}></div>
-      <form className="newForm">
+      <form className="newForm" onSubmit={handleSubmit}>
         {/* <label htmlFor="file">
           <img src="/attachment.png" alt="" />
         </label> */}
         <Upload setImg={setImg} />
         <input id="file" type="file" multiple={false} hidden />
-        <input type="text" placeholder="Ask me anything...." />
+        <input type="text" name="text" placeholder="Ask me anything...." />
         <button>
           <img src="/arrow.png" alt="" />
         </button>
