9954005a6f32e94b1deb0a10cb47793ea06b9ef7
Viswaksena Jayam <viswaksene111@gmail.com>
2024-08-02 11:01:32 +0530

    Add new chat using MongoDB

--- backend/index.js
+++ backend/index.js
@@ -2,6 +2,8 @@ import express from 'express';
 import ImageKit from 'imagekit';
 import cors from 'cors';
 import mongoose from 'mongoose';
+import Chat from "./models/chat.js";
+import UserChats from "./models/userChats.js";
 
 const port = process.env.PORT || 3000;
 const app = express();
@@ -11,6 +13,9 @@ app.use(cors(
         origin: process.env.CLIENT_URL,
     }
 ));
+
+app.use(express.json());
+
 const connect = async() => {
     try {
         await mongoose.connect(process.env.MONGO_URL)
@@ -26,12 +31,71 @@ const imagekit = new ImageKit({
     privateKey: process.env.IMAGE_KIT_PRIVATE_KEY,
 });
 
-app.get("/api/upload", (req, res) => {
-    const result = imagekit.getAuthenticationParameters();
-    res.send(result);
-  });
+app.post("/api/chats", async (req, res) => {
+    const {text, userId} = req.body;
 
+    try {
+        //New Chat Creation
+        const newChat = new Chat({
+            userId : userId,
+            history:[
+                {
+                    role:"user",
+                    parts:[
+                        {
+                            text,
+                        }
+                    ]
+                }
+            ]
+        })
+ 
+        const savedChat = await newChat.save();
+
+        // Check if the user chat exists
+        const userChats = await UserChats.find({userId:userId})
+
+        // Created a array of the user if the user chat does not exist
+        if(!userChats.length){
+            const newUserChats = new UserChats({
+                userId: userId,
+                chats:[
+                    {
+                        _id: savedChat._id,
+                        title: text.substring(0, 40),
+                    }
+                ]
+            });
+            await newUserChats.save();
+        } 
+        // If the user chat exists, push the new chat to the array
+        else{
+            await UserChats.updateOne(
+                {userId: userId},
+                {
+                    $push:{
+                        chats:{
+                            _id: savedChat._id,
+                            title: text.substring(0, 40),
+                        }
+                    }
+                }
+            );
+
+            res.status(201).send(newChat._id)
+        }   
+    } catch (error) {
+        console.log(error)
+        res.status(500).json("Error created chat!")
+        
+    }
+});
+
+app.get('/api/upload',(req,res) => {
+    const result = imagekit.getAuthenticationParameters();
+    res.json(result);
+})
 app.listen(port, ()=> {
     connect();
     console.log(`Server running on ${port}`)
-})
+}) 
